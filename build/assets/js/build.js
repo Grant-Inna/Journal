f16a139cdbe9d4c11fc1c754e0723b0501be4b6c
Grant Inna <grantinna@mail.ru>
2022-08-23 14:03:20 +0300

    editors

--- build/assets/js/build.js
+++ build/assets/js/build.js
@@ -18,6 +18,32 @@ $(document).ready(function() {
 });
 
 $(document).ready(function () {
+   /* читать дальше */
+   
+   if ($('.relevance__holder').length > 0) {
+      
+      let $question_holder = $('.relevance__more'),
+          $hide = $('.hidden');
+      $question_holder.on('click', openAnswer);
+      
+      function openAnswer() {
+         if ($(this).parent().hasClass('open')) {
+            $hide.fadeOut();
+            $(this).parent().removeClass('open');
+            $(this).html('Читать дальше');
+         } else {
+            $hide.fadeIn();
+            $(this).parent().addClass('open');
+            $(this).html('Скрыть');
+         }
+      }
+      
+   }
+   
+   
+});
+
+$(document).ready(function () {
    /* плавный скрол */
    
    if ($('.nav__link').length > 0) {
@@ -58,31 +84,5 @@ $(document).ready(function() {
          centerMode: true,
       });
    }
-   
-});
-
-$(document).ready(function () {
-   /* читать дальше */
-   
-   if ($('.relevance__holder').length > 0) {
-      
-      let $question_holder = $('.relevance__more'),
-          $hide = $('.hidden');
-      $question_holder.on('click', openAnswer);
-      
-      function openAnswer() {
-         if ($(this).parent().hasClass('open')) {
-            $hide.fadeOut();
-            $(this).parent().removeClass('open');
-            $(this).html('Читать дальше');
-         } else {
-            $hide.fadeIn();
-            $(this).parent().addClass('open');
-            $(this).html('Скрыть');
-         }
-      }
-      
-   }
-   
    
 });
