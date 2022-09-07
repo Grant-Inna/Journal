0da58c43d99ffeb9b5b140c225e46142601793d6
Grant Inna <grantinna@mail.ru>
2022-09-07 17:59:52 +0300

    переделали скрипт

--- develop/assets/js/concat/faq/faq.js
+++ develop/assets/js/concat/faq/faq.js
@@ -1,20 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
    /* года */
    
-   if($('.question-holder').length > 0 ) {
+   if ($('.question-holder').length > 0) {
       let $question_holder = $('.question-holder'),
-         $title = $('.question-theme');
+          $title = $('.question-theme');
+      
+      $('.question-holder:first-child').addClass('open');
+      $('.open .answer-text').slideDown(350);
+      
+      // $('.open').find('.answer-text').style('display', 'block');
+      
       $question_holder.find($title).on('click', openAnswer);
    }
    
    function openAnswer() {
-      let answer = $('#question-' + $(this).data('question'));
+      let answer = $('#question-' + $(this).data('question')),
+          s = 500;
       
-      if ($(this).parent().hasClass('open')) {
-         answer.fadeOut();
-         $('.trigger-holder').find('.open').removeClass('open')
-      } else {
-         answer.fadeIn();
+      if (!$(this).parent().hasClass('open')) {
+         $('.open').removeClass('open');
+         $('.answer-text').slideUp(s);
+         answer.slideDown(s);
          $(this).parent().addClass('open')
       }
    }
