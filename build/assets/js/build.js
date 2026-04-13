f353dac2ff20abaf829baa132b338724a5e71d0f
Inna Grant <i.grant@ficto.ru>
2026-04-13 18:05:22 +0300

    feat: новый выпуск №2 (70) 2026

--- build/assets/js/build.js
+++ build/assets/js/build.js
@@ -1,3 +1,86 @@
+$(document).ready(function () {
+   /* года */
+   
+   if ($('.archive__content').length > 0) {
+      let $year_holder = $('.question-holder'),
+          $title = $('.question-theme');
+      
+      let first_year_holder = $('.question-holder:first-child');
+      
+      if (document.location.hash.length == 0 ) { // Если идём через hash, то добавлять классы не нужно
+         first_year_holder.addClass('open');
+         $('.open .answer-text').slideDown(350);
+         $('.open .trigger:last-child').addClass('active_number'); // Подсвечиваем последний выпуск за последний год
+      } else {
+         first_year_holder.removeClass('open'); // Убираем класс и показываем иконку
+      }
+      
+      $year_holder.find($title).on('click', openYear);
+   }
+   
+   function openYear() {
+      let answer = $('#question-' + $(this).data('question')),
+          s = 500;
+      
+      if (!$(this).parent().hasClass('open')) { // Проверка, открыт ли год
+         $('.open').removeClass('open');
+         $('.answer-text').slideUp(s);
+         answer.slideDown(s);
+         $(this).parent().addClass('open')
+      }
+   }
+   
+   /* требования к статье  */
+   
+   if ($('.requirements__list').length > 0) {
+      let $year_holder = $('.question-holder'),
+          $title = $('.question-theme');
+      
+      $year_holder.find($title).on('click', openRequirements);
+   }
+   
+   function openRequirements() {
+      let answer = $('#question-' + $(this).data('question')),
+          s = 500;
+      
+      if (!$(this).parent().hasClass('open')) {
+         $('.open').removeClass('open');
+         $('.answer-text').slideUp(s);
+         answer.slideDown(s);
+         $(this).parent().addClass('open')
+      } else {
+         answer.slideUp(s);
+         $(this).parent().removeClass('open')
+      }
+   }
+});
+
+$(document).ready(function () {
+   /* читать дальше */
+   
+   if ($('.relevance__holder').length > 0) {
+      
+      let $question_holder = $('.relevance__more'),
+          $hide = $('.hidden');
+      $question_holder.on('click', openAnswer);
+      
+      function openAnswer() {
+         if ($(this).parent().hasClass('open')) {
+            $hide.fadeOut();
+            $(this).parent().removeClass('open');
+            $(this).html('Читать дальше');
+         } else {
+            $hide.fadeIn();
+            $(this).parent().addClass('open');
+            $(this).html('Скрыть');
+         }
+      }
+      
+   }
+   
+   
+});
+
 $(document).ready(function() {
    /* манипуляции с журналом */
    
@@ -108,89 +191,6 @@ $(document).ready(function () {
    function closeModal() {
       let $this_overlay = $(this).parents('.modal__overlay');
      $this_overlay.removeClass('visible_modal');
-   }
-   
-   
-});
-
-$(document).ready(function () {
-   /* года */
-   
-   if ($('.archive__content').length > 0) {
-      let $year_holder = $('.question-holder'),
-          $title = $('.question-theme');
-      
-      let first_year_holder = $('.question-holder:first-child');
-      
-      if (document.location.hash.length == 0 ) { // Если идём через hash, то добавлять классы не нужно
-         first_year_holder.addClass('open');
-         $('.open .answer-text').slideDown(350);
-         $('.open .trigger:last-child').addClass('active_number'); // Подсвечиваем последний выпуск за последний год
-      } else {
-         first_year_holder.removeClass('open'); // Убираем класс и показываем иконку
-      }
-      
-      $year_holder.find($title).on('click', openYear);
-   }
-   
-   function openYear() {
-      let answer = $('#question-' + $(this).data('question')),
-          s = 500;
-      
-      if (!$(this).parent().hasClass('open')) { // Проверка, открыт ли год
-         $('.open').removeClass('open');
-         $('.answer-text').slideUp(s);
-         answer.slideDown(s);
-         $(this).parent().addClass('open')
-      }
-   }
-   
-   /* требования к статье  */
-   
-   if ($('.requirements__list').length > 0) {
-      let $year_holder = $('.question-holder'),
-          $title = $('.question-theme');
-      
-      $year_holder.find($title).on('click', openRequirements);
-   }
-   
-   function openRequirements() {
-      let answer = $('#question-' + $(this).data('question')),
-          s = 500;
-      
-      if (!$(this).parent().hasClass('open')) {
-         $('.open').removeClass('open');
-         $('.answer-text').slideUp(s);
-         answer.slideDown(s);
-         $(this).parent().addClass('open')
-      } else {
-         answer.slideUp(s);
-         $(this).parent().removeClass('open')
-      }
-   }
-});
-
-$(document).ready(function () {
-   /* читать дальше */
-   
-   if ($('.relevance__holder').length > 0) {
-      
-      let $question_holder = $('.relevance__more'),
-          $hide = $('.hidden');
-      $question_holder.on('click', openAnswer);
-      
-      function openAnswer() {
-         if ($(this).parent().hasClass('open')) {
-            $hide.fadeOut();
-            $(this).parent().removeClass('open');
-            $(this).html('Читать дальше');
-         } else {
-            $hide.fadeIn();
-            $(this).parent().addClass('open');
-            $(this).html('Скрыть');
-         }
-      }
-      
    }
    
    
